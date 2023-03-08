fb282df1d6f293c255fc4dbcee702612b7d7e0be
Anonymous-XI <127212152+TheAnonymousXI@users.noreply.github.com>
2023-03-08 18:09:57 -0500

    Update V4.js

--- V4.js
+++ V4.js
@@ -1,19 +1,20 @@
-javascript:!function(){var c=0x1f4,d=0x12c,e='#AAA',f=0x1,g=0x20,h='#444',i='#FFF',j='Bookmarklets',k=~~(document['documentElement']['clientWidth']/0x2-c/0x2),l=~~(document['documentElement']['clientHeight']/0x2-d/0x2),m=~~(0.8*g),
-n=document['createElement']('DIV');Object['assign'](n['style'],{'position':'absolute','left':k+'px','top':l+'px','zIndex':Number['MAX_SAFE_INTEGER'],'width':c+'px','height':d+'px','background':e,'border':f+'px\x20solid\x20black'});var%20o=document['createElement']('DIV');Object['assign'](o['style'],{'position':'relative','width':c+'px','height':g+'px','background':h,'borderBottom':f+'px\x20solid\x20black'});var%20p=document['createElement']('DIV');Object['assign'](p['style'],{'position':'relative','display':'inline-block','left':0x0,'width':~~(c-0x2*m)+'px','lineHeight':g+'px','color':i,'fontSize':~~(0.667*g)+'px','marginLeft':~~(m/0x3)+'px'}),p['textContent']=j;var%20q=document['createElement']('DIV'),r=~~((g-m)/0x2);Object['assign'](q['style'],{'position':'relative','float':'right','right':r+'px','top':r+'px','width':m+'px','height':m+'px','background':'#F00','border':f+'px\x20solid\x20black','color':'#FFF','lineHeight':m+'px','textAlign':'center','fontSize':m+'px','marginLeft':'auto','marginRight':0x0});var%20s=document['createElement']('DIV');Object['assign'](s['style'],{'padding':'1em'});var%20t=document['createElement']('P');t['textContent']='Click\x20them\x20to\x20inject\x20them\x20into\x20your\x20tab!',s['appendChild'](t);var%20u=document['createElement']('UL');[
-
-
-
-//{'name: 'Name','url':'js:Javascript'},
-
-
-{'name':'Blooket Hacks','url':' '},
-
-
-{'name':'Bing','url':'https://www.bing.com'},
-
-
-{'name':'DuckDuckGO','url':'https://duckduckgo.com'}
-
-
-
-]['forEach'](c=%3E{var%20d=document['createElement']('LI'),e=document['createElement']('A');e['setAttribute']('href',c['url']),e['setAttribute']('target','_blank'),e['textContent']=c['name'],d['appendChild'](e),u['appendChild'](d);}),s['appendChild'](u),q['addEventListener']('click',function%20c(d){q['removeEventListener']('click',c,!0x1);o['removeChild'](q);n['removeChild'](o);n['removeChild'](s);document['body']['removeChild'](n);},!0x1),q['textContent']='%C3%97',o['appendChild'](p),o['appendChild'](q),n['appendChild'](o),n['appendChild'](s),document['body']['appendChild'](n),function(c){var%20d=function(c){var%20d=c['getBoundingClientRect'](),e=window['pageXOffset']||document['documentElement']['scrollLeft'],f=window['pageYOffset']||document['documentElement']['scrollTop'];return{'top':d['top']+f,'left':d['left']+e};}(c['parentElement']),e=!0x1,f={'x':0x0,'y':0x0},g={'x':d['left'],'y':d['top']};c['parentElement']['addEventListener']('mousedown',function(d){e=!0x0,f['x']=d['clientX'],f['y']=d['clientY'],c['parentElement']['style']['cursor']='move';}),c['parentElement']['addEventListener']('mouseup',function(d){e=!0x1,g['x']=parseInt(c['parentElement']['style']['left'])||0x0,g['y']=parseInt(c['parentElement']['style']['top'])||0x0,c['parentElement']['style']['cursor']='auto';}),document['addEventListener']('mousemove',function(d){if(!e)return;var%20h={'x':d['clientX']-f['x'],'y':d['clientY']-f['y']},i={'x':g['x']+h['x'],'y':g['y']+h['y']};i['x']%3C0x0?i['x']=0x0:i['x']+c['parentElement']['offsetWidth']%3Edocument['documentElement']['clientWidth']&&(i['x']=document['documentElement']['clientWidth']-c['parentElement']['offsetWidth']);i['y']%3C0x0?i['y']=0x0:i['y']+c['parentElement']['offsetHeight']%3Edocument['documentElement']['clientHeight']&&(i['y']=document['documentElement']['clientHeight']-c['parentElement']['offsetHeight'];c['parentElement']['style']['left']=i['x']+'px',c['parentElement']['style']['top']=i['y']+'px';});}(o);}(window);
+<div style="position:absolute; z-index:2147483647">
+  <div style="position: relative">
+    <div style="position:relative; display:inline-block; left:0">Bookmarklet Links</div>
+    <div style="position:relative; float:right">×</div>
+  </div>
+  <div>
+    <p>Click the links to open a new tab!</p>
+    <ul>
+      <li>
+        <a href="https://www.google.com" target="_blank">Google</a>
+      </li>
+      <li>
+        <a href="https://www.bing.com" target="_blank">Bing</a>
+      </li>
+      <li>
+        <a href="https://duckduckgo.com" target="_blank">DuckDuckGO</a>
+      </li>
+    </ul>
+  </div>
+</div>
