fb2616cee58c25b008c6b82db987d062554bf24c
Anonymous-XI <127212152+TheAnonymousXI@users.noreply.github.com>
2023-03-10 12:38:02 -0500

    Update V4.js

--- V4.js
+++ V4.js
@@ -59,7 +59,7 @@ javascripts:
 				url: "js:Title"
 			},
 			{
-				name: "Page Nucker",
+				name: "Page Nuker",
 				url: "javascript:var KICKASSVERSION='2.0';var s = document.createElement('script');s.type='text/javascript';document.body.appendChild(s);s.src='//hi.kickassapp.com/kickass.js';void(0);"
 			},
 			{
